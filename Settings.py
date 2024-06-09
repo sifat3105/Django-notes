3905f3743797620a87c1e91395e001be1390f87f
MD SIFAT ALI <162755505+sifat3105@users.noreply.github.com>
2024-06-10 02:57:12 +0600

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -31,8 +31,8 @@ EMAIL_BackEND = 'django.care.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sifatali360@gmail.com'
-EMAIL_HOST_PASSWORD = 'huhe rrbm klcz nfdv'
+EMAIL_HOST_USER = 'exmaple@gmail.com'
+EMAIL_HOST_PASSWORD = 'fujf grsf fead dwdd'
 
 
 #________________________ MIDDLEWARE ______________
